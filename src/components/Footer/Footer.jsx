7d5b32e66bf7419c5a35e12c845e647830337cb0
Teddy Medjio <nganmedjiojeanclaude@gmail.com>
2023-03-26 16:07:23 +0100

    my first commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,30 @@
+import { Logo } from "@components/Logo/Logo";
+
+const Footer = () => (
+  <footer className="bg-brand-gray">
+    <div className="py-12 lg:pt-16 flex flex-col justify-center">
+      <Logo className="h-31" color="#758896" text="#758896" />
+      <p className="mt-3 max-w-4xl mx-auto text-center text-white text-2xl">
+        <span className="text-brand-light-gray">Adresse:</span> Étude de la
+        notaire Riegelová Rue de Nidau 14 2502 Biel/Bienne{" "}
+        <span className="text-brand-light-gray">Phone:</span> +41 (0) 32 322 43
+        55 - <span className="text-brand-light-gray">Email:</span>{" "}
+        info[@]notariat-biel.ch
+      </p>
+    </div>
+    <div className="border-t border-brand-light-gray py-4">
+      <p className="text-center text-slate-400">
+        Copyright {new Date().getFullYear()} Notariat Riegelová | Website by{" "}
+        <a
+          href="https://medjio.me"
+          className="text-white font-medium"
+          target="_blank"
+        >
+          nj
+        </a>
+      </p>
+    </div>
+  </footer>
+);
+
+export default Footer;
---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          red: "#9F1832",
+          "light-red": "#B4203D",
+          blue: "#002654",
+          yellow: "#F4AC45",
+          gray: "#2B3B45",
+          "light-gray": "#758896",
+        },
+      },
+      fontFamily: {
+        body: ["Inter", ...defaultTheme.fontFamily.sans],
+        heading: ["Vidaloka", "sans-serif"],
+      },
+      spacing: {
+        18: "4.5rem",
+        31: "7.75rem",
+        54: "13.5rem",
+      },
+      fontSize: {
+        "2xl": ["1.625rem", { lineHeight: "2rem" }],
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/forms")],
+};
