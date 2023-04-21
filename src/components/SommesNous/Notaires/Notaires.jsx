2f8212f297c3e484d343dabd46b50fd58f851fdf
Teddy Medjio <nganmedjiojeanclaude@gmail.com>
2023-04-21 15:52:22 +0100

    correctifs

--- src/components/SommesNous/Notaires/Notaires.jsx
+++ src/components/SommesNous/Notaires/Notaires.jsx
@@ -13,7 +13,7 @@ export default function Notaires() {
         >
           <div className="py-10 bg-gradient-to-t from-brand-red w-full px-5 rounded-b-lg lg:opacity-0 transition-all duration-1000 ease-in-out  group-hover:opacity-100 lg:group-hover:translate-y-1">
             <div className=" flex items-center pt-20 ">
-              <h2 className="text-5xl text-white font-heading mr-5 group-hover:opacity-100 translate-x-10 lg:group-hover:translate-x-0 transition-all duration-1000">
+              <h2 className="text-5xl text-white font-heading mr-5 group-hover:opacity-100 lg:translate-x-10 lg:group-hover:translate-x-0 transition-all duration-1000">
                 Barbora <br className="md:hidden" /> Riegelová
               </h2>
               <div className="relative">
@@ -21,10 +21,10 @@ export default function Notaires() {
                 <span className="bg-yellow-500 h-12 w-12 rounded-full absolute top-0 animate-ping lg:hidden"></span>
               </div>
             </div>
-            <p className=" text-2xl text-white mt-3 border-brand-yellow border-b group-hover:opacity-100 translate-x-18 lg:group-hover:translate-x-0 transition-all duration-1000">
+            <p className=" text-2xl text-white mt-3 border-brand-yellow border-b group-hover:opacity-100 lg:translate-x-18 lg:group-hover:translate-x-0 transition-all duration-1000">
               Master of law, notaire du canton de Berne
             </p>
-            <p className="text-brand-yellow text-xl group-hover:opacity-100 -translate-x-28 lg:group-hover:translate-x-0 transition-all duration-1000 ">
+            <p className="text-brand-yellow text-xl group-hover:opacity-100 lg:-translate-x-28 lg:group-hover:translate-x-0 transition-all duration-1000 ">
               info@notariat-biel.ch
             </p>
           </div>
@@ -37,7 +37,7 @@ export default function Notaires() {
         >
           <div className="py-10 bg-gradient-to-t from-brand-red w-full px-5 rounded-b-lg lg:opacity-0 transition-all duration-1000 ease-in-out  group-hover:opacity-100 lg:group-hover:translate-y-1">
             <div className=" flex items-center pt-20 ">
-              <h2 className="text-5xl text-white font-heading mr-5 group-hover:opacity-100 translate-x-10 lg:group-hover:translate-x-0 transition-all duration-1000">
+              <h2 className="text-5xl text-white font-heading mr-5 group-hover:opacity-100 lg:translate-x-10 lg:group-hover:translate-x-0 transition-all duration-1000">
                 Regula <br className="md:hidden" /> Stettler
               </h2>
               <div className="relative">
@@ -45,10 +45,10 @@ export default function Notaires() {
                 <span className="bg-yellow-500 h-12 w-12 rounded-full absolute top-0 animate-ping lg:hidden"></span>
               </div>
             </div>
-            <p className=" text-2xl text-white mt-3 border-brand-yellow border-b group-hover:opacity-100 translate-x-18 lg:group-hover:translate-x-0 transition-all duration-1000">
+            <p className=" text-2xl text-white mt-3 border-brand-yellow border-b group-hover:opacity-100 lg:translate-x-18 lg:group-hover:translate-x-0 transition-all duration-1000">
               Employée de notaire certifiée
             </p>
-            <p className="text-brand-yellow text-xl group-hover:opacity-100 -translate-x-28 lg:group-hover:translate-x-0 transition-all duration-1000 ">
+            <p className="text-brand-yellow text-xl group-hover:opacity-100 lg:-translate-x-28 lg:group-hover:translate-x-0 transition-all duration-1000 ">
               regula.stettler@notariat-biel.ch
             </p>
           </div>
@@ -61,7 +61,7 @@ export default function Notaires() {
         >
           <div className="py-10 bg-gradient-to-t from-brand-red w-full px-5 rounded-b-lg lg:opacity-0 transition-all duration-1000 ease-in-out  group-hover:opacity-100 lg:group-hover:translate-y-1">
             <div className=" flex items-center pt-20 ">
-              <h2 className="text-5xl text-white font-heading mr-5 group-hover:opacity-100 translate-x-10 lg:group-hover:translate-x-0 transition-all duration-1000">
+              <h2 className="text-5xl text-white font-heading mr-5 group-hover:opacity-100 lg:translate-x-10 lg:group-hover:translate-x-0 transition-all duration-1000">
                 Maria <br className="md:hidden" /> Righenzi
               </h2>
               <div className="relative">
@@ -69,10 +69,10 @@ export default function Notaires() {
                 <span className="bg-yellow-500 h-12 w-12 rounded-full absolute top-0 animate-ping lg:hidden"></span>
               </div>
             </div>
-            <p className=" text-2xl text-white mt-3 border-brand-yellow border-b group-hover:opacity-100 translate-x-18 lg:group-hover:translate-x-0 transition-all duration-1000">
+            <p className=" text-2xl text-white mt-3 border-brand-yellow border-b group-hover:opacity-100 lg:translate-x-18 lg:group-hover:translate-x-0 transition-all duration-1000">
               Employée de notaire certifiée
             </p>
-            <p className="text-brand-yellow text-xl group-hover:opacity-100 -translate-x-28 lg:group-hover:translate-x-0 transition-all duration-1000 ">
+            <p className="text-brand-yellow text-xl group-hover:opacity-100 lg:-translate-x-28 lg:group-hover:translate-x-0 transition-all duration-1000 ">
               maria.righenzi@notariat-biel.ch
             </p>
           </div>
@@ -85,7 +85,7 @@ export default function Notaires() {
         >
           <div className="py-10 bg-gradient-to-t from-brand-red w-full px-5 rounded-b-lg lg:opacity-0 transition-all duration-1000 ease-in-out  group-hover:opacity-100 lg:group-hover:translate-y-1">
             <div className=" flex items-center pt-20 ">
-              <h2 className="text-5xl text-white font-heading mr-5 group-hover:opacity-100 translate-x-10 lg:group-hover:translate-x-0 transition-all duration-1000">
+              <h2 className="text-5xl text-white font-heading mr-5 group-hover:opacity-100 lg:translate-x-10 lg:group-hover:translate-x-0 transition-all duration-1000">
                 Carla <br className="md:hidden" /> Hartmann
               </h2>
               <div className="relative">
@@ -93,10 +93,10 @@ export default function Notaires() {
                 <span className="bg-yellow-500 h-12 w-12 rounded-full absolute top-0 animate-ping lg:hidden"></span>
               </div>
             </div>
-            <p className=" text-2xl text-white mt-3 border-brand-yellow border-b group-hover:opacity-100 translate-x-18 lg:group-hover:translate-x-0 transition-all duration-1000">
+            <p className=" text-2xl text-white mt-3 border-brand-yellow border-b group-hover:opacity-100 lg:translate-x-18 lg:group-hover:translate-x-0 transition-all duration-1000">
               Assistante juridique
             </p>
-            <p className="text-brand-yellow text-xl group-hover:opacity-100 -translate-x-28 lg:group-hover:translate-x-0 transition-all duration-1000 ">
+            <p className="text-brand-yellow text-xl group-hover:opacity-100 lg:-translate-x-28 lg:group-hover:translate-x-0 transition-all duration-1000 ">
               sekretariat@notariat-biel.ch
             </p>
           </div>
